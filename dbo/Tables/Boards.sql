3049282edbdbf1943c8378d8de3017a548876597
Jaeyoung <69396761+Jaeyoungzairong@users.noreply.github.com>
2020-08-21 13:45:25 +0900

    Add files via upload

---
+++ dbo/Tables/Boards.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Boards]
+(
+	Id Int Identity(1, 1) Not Null PRIMARY KEY,
+	Name NVarChar(25) Not Null,
+	Email NVarChar(100) Not Null,
+	Title NVarChar(150) Not Null,
+	PostDate DateTime Default GetDate() Not Null,
+	PostIp NVarChar(15) Null,
+	Content NText Not Null,
+	Password NVarChar(20) Null,
+	ReadCount Int Default 0,
+	Encoding NVarChar(10) Null,
+	Homepage NVarChar(100) Null,
+	ModifyDate DateTime Not Null,
+	ModifyIp NVarChar(15) Null
+)
