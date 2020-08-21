cd73dbadffb410abb5eeed480a33bf5ad6c9d553
Jaeyoung <69396761+Jaeyoungzairong@users.noreply.github.com>
2020-08-21 13:38:58 +0900

    Delete Procedure1.sql

--- dbo/Tables/Procedure1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[Procedure1]
-	@param1 int = 0,
-	@param2 int
-AS
-	SELECT [Id], [Name], [Email], [Title], [PostDate], [PostIp], [Content], [Password], [ReadCount], [Encoding], [Homepage], [ModifyDate], [ModifyIp] FROM Boards
-RETURN 0
